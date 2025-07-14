40b78726dd1c69fe041d86814b3d6f39c0d3b347
Yuliia <shevchuuchkaaa@gmail.com>
2025-07-14 14:49:14 +0200

    solution

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Header {
-
+  get logo() {
+    return 'Conduit logo';
+  }
 };
 
 class PageObject {
+  url = 'http://test.com';
+  header = new Header();
 
+  clickOnLogo() {
+    return `Click on the ${this.header.logo}`;
+  }
 };
 
 class ArticlePage extends PageObject {
+  slug = '/article';
+  url = this.url + this.slug;
 
+  get commentButton() {
+    return '[Publish comment] button';
+  }
+
+  clickOnCommentButton() {
+    return `Click on the ${this.commentButton}`;
+  }
+
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
 };
 
 module.exports = {
